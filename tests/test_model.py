cb21efda075b26b01ce8d7ae3ecadac47f14ea7c
satyanarayana8055 <muddalasatyanarayana96@gmail.com>
2025-07-08 08:13:44 +0530

    api

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,12 +1,12 @@
 from config.config import Config
-
+import os
 
 def test_model_path_exists():
     assert Config.MODEL_PATH.exists(), f"Model path does not exist: {Config.MODEL_PATH}"
 
 
 def test_base_model_exists():
-    base_model = Config.MODEL_PATH / "yolov5m.pt"
+    base_model = os.path.join(Config.MODEL_PATH , "yolov5m.pt")
     assert base_model.exists(), f"Pretrained model not found: {base_model}"
 
 
